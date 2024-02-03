1f64ec9c48433ae98afeeb38edc00fee87f7dd77
Weasly <artem.ryzhkov208@bk.ru>
2024-02-03 12:25:06 +0500

    first commit

---
+++ Practice4/Practice4.cpp
@@ -0,0 +1,29 @@
+﻿// Practice4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    FILE* inputFile;
+
+    if (fopen_s(&inputFile, "coordinates.txt", "r") != 0) {
+        std::cout << "Невозможно открыть файл" << std::endl;
+        return -1;
+    }
+    
+    float x, y, z;
+    fscanf_s(inputFile, "%f %f %f", &x, &y, &z);
+
+    std::fclose(inputFile);
+
+    std::cout << "Координаты: (" << x << ", " << y << ", " << z << ")" << std::endl;
+    double distance = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    std::cout << "Расстояние от начала координат до точки: " << distance << std::endl;
+
+    return 0;
+}
+
